4402c0d8b002b23ffe96eaad40fd786fa4ae3400
Wiloux <54811449+Wiloux@users.noreply.github.com>
2021-04-17 14:35:47 +0200

    Enemy

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -7,31 +7,40 @@ public class EnemyScript : MonoBehaviour
 
     public PlayerMovement player;
     public Animator anim;
+    private Rigidbody2D rb;
     public bool isFalling;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player>().GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFalling)
+        {
+            Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
     }
 
     void Fall()
     {
-
+        anim.SetTrigger("Fall");
+        isFalling = true;
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
+        ContactPoint2D contact = other.GetContact(0);
         if (other.transform.tag == "Player")
         {
             if (!isFalling)
             {
                 if (player.isTackling)
                 {
+                    rb.AddForce(new Vector2(Random.Range(50,65),40), ForceMode2D.Impulse);
                     Fall();
                 }
                 else
@@ -40,15 +49,22 @@ public class EnemyScript : MonoBehaviour
                 }
             }
         }
-        Vector3 direction = other.transform.position - transform.position;
-        Debug.Log(direction);
-        if (Vector2.Dot(transform.forward, direction) > 0)
+
+        if (other.transform.tag == "Ground")
         {
-            print("Back");
-        }
-        if (Vector2.Dot(transform.forward, direction) == 0)
-        {
-            print("Side");
+            if (isFalling)
+            {
+                Vector2 direction = contact.point - new Vector2(transform.position.x, transform.position.y);
+                if (direction.x > 0)
+                {
+                    anim.SetTrigger("Wall");
+                }
+
+                if (direction.y < 0)
+                {
+                    anim.SetTrigger("Ground");
+                }
+            }
         }
     }
 }
