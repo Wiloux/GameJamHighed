989030af5b8c71571f5d0050598b9ecd25e7021b
Outfoxeed <letourneaumorigan@gmail.com>
2021-04-17 22:14:32 +0200

    Starting UI and score system

--- Assets/Scripts/Handling/UIHandler.cs
+++ Assets/Scripts/Handling/UIHandler.cs
@@ -6,7 +6,14 @@ using TMPro;
 
 public class UIHandler : MonoBehaviour
 {
+    public enum Mode { playing, pause, options};
+    private Mode mode;
+
     [SerializeField] TMP_Text scoreDisplayer;
+
+    [SerializeField] GameObject gameUI;
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject optionsMenu;
 
     GameHandler gameHandler;
     PlayerHelper playerHelper;
@@ -22,5 +29,46 @@ public class UIHandler : MonoBehaviour
     void Update()
     {
         scoreDisplayer.text = "Score: " + ((int)playerHelper.Score).ToString();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Escape();
+        }
     }
+
+    public void Escape()
+    {
+        switch (mode)
+        {
+            case Mode.playing:
+                gameUI.SetActive(false);
+                pauseMenu.SetActive(true);
+                mode = Mode.pause;
+                break;
+            case Mode.pause:
+                gameUI.SetActive(true);
+                pauseMenu.SetActive(false);
+                gameHandler.SetPause(false);
+                mode = Mode.playing;
+                break;
+            case Mode.options:
+                pauseMenu.SetActive(true);
+                optionsMenu.SetActive(false);
+                mode = Mode.pause;
+                break;
+        }
+    }
+
+    public void OpenOptionsFromPauseMenu()
+    {
+        optionsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        mode = Mode.options;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
